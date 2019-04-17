48cbfc7a1afd2aecb148335688b10555f9a03ee6
gr64 <31944054+gr64@users.noreply.github.com>
2019-04-16 21:57:25 -0400

    Update ServoTest.v

--- ServoTest.v
+++ ServoTest.v
@@ -1,13 +1,16 @@
-module ServoTest(left,right,rst,clk,servoin); 
+module ServoTest(s1,s2,s3,s4,rst,clk,servo1,servo2,servo3,servo4); 
 	
-	input rst,left,right;
+	input rst,s1,s2,s3,s4;
 	input clk;
-	output reg servoin;
+	output reg servo1,servo2,servo3,servo4;
 	reg [19:0] counter;
 	
 	initial begin
-	servoin <= 1'b1;
-	counter <= 20'b0;
+		servo1 <= 1'b1;
+		servo2 <= 1'b1;
+		servo3 <= 1'b1;
+		servo4 <= 1'b1;
+		counter <= 20'b0;
 	end
 	
 	always @(posedge clk) begin
@@ -16,25 +19,60 @@ module ServoTest(left,right,rst,clk,servoin);
 		end
 		else if (counter >= 20'd1000000) begin
 			counter <= 20'b0;
-			servoin <= 1'b1;
+			servo1 <= 1'b1;
+			servo2 <= 1'b1;
+			servo3 <= 1'b1;
+			servo4 <= 1'b1;
 		end
-		else if((right & left) | (!right & !left)) begin
-			if(counter >= 70000) begin
-				servoin <= 1'b0;
+		else begin
+		
+			//SERVO 1
+			if(~s1) begin
+				if(counter >= 70000) begin
+					servo1 <= 1'b0;
+				end
 			end
-		end                                
-		else if(right) begin
-			if(counter >= 112500) begin
-				servoin <= 1'b0;
+			else if(s1) begin
+				if(counter >= 5000) begin
+					servo1 <= 1'b0;
+				end
 			end
+			//SERVO 2
+			if(~s2) begin
+				if(counter >= 70000) begin
+					servo2 <= 1'b0;
+				end
+			end
+			else if(s2) begin
+				if(counter >= 5000) begin
+					servo2 <= 1'b0;
+				end
+			end
+			//SERVO 3
+			if(~s3) begin
+				if(counter >= 70000) begin
+					servo3 <= 1'b0;
+				end
+			end
+			else if(s3) begin
+				if(counter >= 5000) begin
+					servo3 <= 1'b0;
+				end
+			end
+			//SERVO 4
+			if(~s4) begin
+				if(counter >= 70000) begin
+					servo4 <= 1'b0;
+				end
+			end
+			else if(s4) begin
+				if(counter >= 5000) begin
+					servo4 <= 1'b0;
+				end
+			end                      
+			counter <= counter + 20'd1;
 		end
-		else if(left) begin
-			if(counter >= 37500) begin
-				servoin <= 1'b0;
-			end
-		end
-		counter <= counter + 20'd1;
    end
 	
-endmodule
+endmodule      
 	                                                    
