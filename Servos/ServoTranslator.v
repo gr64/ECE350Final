6f9fb1f151e3f69a4af8afeb5fee9004ae32b7f4
Gabriela Rodriguez-Florido <gr64@duke.edu>
2019-04-20 20:04:53 -0400

    added 2 more cases

--- Servos/ServoTranslator.v
+++ Servos/ServoTranslator.v
@@ -35,6 +35,7 @@ module ServoTranslator(dmem_out,enable, clk, rst,
 		global_count_max <= 27'd100000000;
 		local_count_1 <= 6'b0;
 		local_count_2 <= 6'b0;
+		local_count_3 <= 6'b0;
 		
 		//assign values to each servo state
 		//outer servo states
@@ -81,7 +82,7 @@ module ServoTranslator(dmem_out,enable, clk, rst,
 				local_count_1 <= 6'd1;
 			end
 				
-			if((global_count >= global_count_max) & (local_count_1 == 1)) begin
+			else if((global_count >= global_count_max) & (local_count_1 == 1)) begin
 				//Rotate Left Servo 90 cc
 				left_servo_rotate <= 1'b1;
 				
@@ -90,7 +91,7 @@ module ServoTranslator(dmem_out,enable, clk, rst,
 					
 			end
 			
-			if((global_count >= global_count_max) & (local_count_1 == 2)) begin
+			else if((global_count >= global_count_max) & (local_count_1 == 2)) begin
 				//Extend upper and lower servos
 				top_servo_forward <= forward;
 				bottom_servo_forward <= forward;
@@ -101,7 +102,7 @@ module ServoTranslator(dmem_out,enable, clk, rst,
 			end
 			
 			
-			if((global_count >= global_count_max) & (local_count_1 == 3)) begin
+			else if((global_count >= global_count_max) & (local_count_1 == 3)) begin
 				//restore Left servo to resting position so first move it back
 				left_servo_forward <= back;
 				
@@ -110,7 +111,7 @@ module ServoTranslator(dmem_out,enable, clk, rst,
 					
 			end
 			
-			if((global_count >= global_count_max) & (local_count_1 == 4)) begin
+			else if((global_count >= global_count_max) & (local_count_1 == 4)) begin
 				//restore Left servo to resting position so next rotate it back
 				left_servo_rotate <= resting;
 				
@@ -119,7 +120,7 @@ module ServoTranslator(dmem_out,enable, clk, rst,
 					
 			end
 			
-			if((global_count >= global_count_max) & (local_count_1 == 5)) begin
+			else if((global_count >= global_count_max) & (local_count_1 == 5)) begin
 				//restore Left servo to resting position so next move it forward
 				left_servo_forward <= forward;
 				
@@ -140,112 +141,87 @@ module ServoTranslator(dmem_out,enable, clk, rst,
 				global_count <= 0;
 				local_count_2 <= local_count_2 + 6'b1;
 			end
-				
-			if((global_count >= global_count_max) & (local_count_2 == 1)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 1)) begin
 				//Rotate Left Servo 90 c --> 270 cc (1)
 				left_servo_rotate <= ccw;	
 				
 				global_count <= 0;
-				local_count_2 <= local_count_2 + 6'b1;
-					
+				local_count_2 <= local_count_2 + 6'b1;	
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 2)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 2)) begin
 				//Extend upper and lower servos
 				top_servo_forward <= forward;
 				bottom_servo_forward <= forward;
 				
 				global_count <= 0;
 				local_count_2 <= local_count_2 + 6'b1;
-					
 			end
-			
-			
-			if((global_count >= global_count_max) & (local_count_2 == 3)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 3)) begin
 				//restore Left servo to resting position so first move it back
 				left_servo_forward <= back;
 				
 				global_count <= 0;
 				local_count_2 <= local_count_2 + 6'b1;
-					
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 4)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 4)) begin
 				//restore Left servo to resting position so next rotate it back
 				left_servo_rotate <= resting;
 				
 				global_count <= 0;
-				local_count_2 <= local_count_2 + 6'b1;
-					
+				local_count_2 <= local_count_2 + 6'b1;	
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 5)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 5)) begin
 				//move left servo back in
 				left_servo_forward <= forward;
 				
 				global_count <= 0;
 				local_count_2 <= local_count_2 + 6'b1;
-					
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 6)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 6)) begin
 				//move left servo back in
 				top_servo_forward <= back;
 				bottom_servo_forward <= back;
 				
 				global_count <= 0;
 				local_count_2 <= local_count_2 + 6'b1;
-					
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 7)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 7)) begin
 				//Rotate Left Servo 90 c --> 270 cc (2)
 				left_servo_rotate <= ccw;	
 				
 				global_count <= 0;
-				local_count_2 <= local_count_2 + 6'b1;
-					
+				local_count_2 <= local_count_2 + 6'b1;	
 			end
-			
-			
-			if((global_count >= global_count_max) & (local_count_2 == 8)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 8)) begin
 				//restore Left servo to resting position so first move it back
 				left_servo_forward <= back;
 				
 				global_count <= 0;
 				local_count_2 <= local_count_2 + 6'b1;
-					
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 9)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 9)) begin
 				//restore Left servo to resting position so next rotate it back
 				left_servo_rotate <= resting;
 				
 				global_count <= 0;
-				local_count_2 <= local_count_2 + 6'b1;
-					
+				local_count_2 <= local_count_2 + 6'b1;	
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 10)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 10)) begin
 				//move left servo back in
 				left_servo_forward <= forward;
 				
 				global_count <= 0;
 				local_count_2 <= local_count_2 + 6'b1;
-					
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 11)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 11)) begin
 				//move left servo back in
 				top_servo_forward <= back;
 				bottom_servo_forward <= back;
 				
 				global_count <= 0;
 				local_count_2 <= local_count_2 + 6'b1;
-					
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 12)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 12)) begin
 				//Rotate Left Servo 90 c --> 270 cc (4)
 				left_servo_rotate <= ccw;	
 				
@@ -253,40 +229,217 @@ module ServoTranslator(dmem_out,enable, clk, rst,
 				local_count_2 <= local_count_2 + 6'b1;
 					
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 13)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 13)) begin
 				//restore Left servo to resting position so first move it back
 				left_servo_forward <= back;
 				
 				global_count <= 0;
 				local_count_2 <= local_count_2 + 6'b1;
-					
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 14)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 14)) begin
 				//restore Left servo to resting position so next rotate it back
 				left_servo_rotate <= resting;
 				
 				global_count <= 0;
-				local_count_2 <= 13;
-					
+				local_count_2 <= 13;	
 			end
-			
-			if((global_count >= global_count_max) & (local_count_2 == 15)) begin
+			else if((global_count >= global_count_max) & (local_count_2 == 15)) begin
 				//restore Left servo to resting position so next move it back forward
 				left_servo_forward <= forward;
 				
 				global_count <= 0;
-				local_count_2 <= 0;
-					
+				local_count_2 <= 0;	
 			end
 			
 		end
+		// MOVE: R
+		else if(dmem_out >= 4'b0010) begin
+			if((global_count >= global_count_max) & (local_count_3 == 0)) begin
+				//retract up and down
+				top_servo_forward <= back;
+				bottom_servo_forward <= back;
+				
+				local_count_3 <= 6'd1;
+				global_count <= 0;
+			end
+			else if((global_count >= global_count_max) & (local_count_3 == 1)) begin
+				//rotate right counter clockwise
+				right_servo_rotate = ccw;
+				
+				local_count_3 <= 6'd2;
+				global_count <=0 ;
+			end
+			else if((global_count >= global_count_max) & (local_count_3 == 2)) begin
+				//extend up and down
+				top_servo_forward <= forward;
+				bottom_servo_forward <= forward;
+				
+				local_count_3 <= 6'b3;
+				global_count <= 0;
+			end
+			else if((global_count >= global_count_max) & (local_count_3 == 3)) begin
+				//retract right servo
+				right_servo_forward <= back;
+				
+				local_count_3 <= 6'b4;
+				global_count <= 0;
+			end
+			else if((global_count >= global_count_max) & (local_count_3 == 4)) begin
+				//turn right servo back
+				right_servo_rotate <= resting;
+				
+				local_count_3 <= 6'b5;
+				global_count <= 0;
+			end
+			else if((global_count >= global_count_max) & (local_count_3 == 5)) begin
+				//go back to resting position by extending right servo back
+				right_servo_forward <= forward;
+				
+				local_count_3 <= 6'b0;
+				global_count <= 0;
+			end
+		end
 		
-		//	else if(dmem_out >= 4'b0010) begin
-		//		if
+		// Move R'
+		else if(dmem_out >= 4'b0011) begin
+			if((global_count >= global_count_max) & (local_count_4 == 0)) begin
+				//Retract Up and Down
+				top_servo_forward <= back;
+				bottom_servo_forward <= back;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd1;
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 1)) begin
+				//Rotate Right Servo 90 c --> 270 cc (1)
+				right_servo_rotate <= ccw;	
+				
+				global_count <= 0;
+				local_count_4 <= 6'd2;	
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 2)) begin
+				//Extend upper and lower servos
+				top_servo_forward <= forward;
+				bottom_servo_forward <= forward;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd3;	
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 3)) begin
+				//restore Right servo to resting position so first move it back
+				right_servo_forward <= back;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd4;
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 4)) begin
+				//restore right servo to resting position so next rotate it back
+				left_servo_rotate <= resting;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd5;
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 5)) begin
+				//move right servo back in
+				right_servo_forward <= forward;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd6;	
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 6)) begin
+				//move top and bottom servos out
+				top_servo_forward <= back;
+				bottom_servo_forward <= back;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd7;	
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 7)) begin
+				//Rotate Right Servo 90 c --> 270 cc (2)
+				right_servo_rotate <= ccw;	
+				
+				global_count <= 0;
+				local_count_2 <= 6'd8;
+					
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 8)) begin
+				//Extend upper and lower servos to hold cube
+				top_servo_forward <= forward;
+				bottom_servo_forward <= forward;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd9;	
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 9)) begin
+				//restore right servo to resting position so first move it back
+				right_servo_forward <= back;
+				
+				global_count <= 0;
+				local_count_4 <= 1'd10;
+					
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 10)) begin
+				//restore Right servo to resting position so next rotate it back
+				right_servo_rotate <= resting;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd11;
+					
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 11)) begin
+				//move right servo back in
+				right_servo_forward <= forward;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd12;		
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 12)) begin
+				//move up and down out
+				top_servo_forward <= back;
+				bottom_servo_forward <= back;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd13;
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 13)) begin
+				//Rotate right Servo 90 c --> 270 cc (3)
+				right_servo_rotate <= ccw;	
+				
+				global_count <= 0;
+				local_count_4 <= 6'd14;
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 14)) begin
+				//Extend upper and lower servos
+				top_servo_forward <= forward;
+				bottom_servo_forward <= forward;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd15;	
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 15)) begin
+				//restore right servo to resting position so first move it back
+				right_servo_forward <= back;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd16;
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 16)) begin
+				//restore right servo to resting position so next rotate it back
+				right_servo_rotate <= resting;
+				
+				global_count <= 0;
+				local_count_4 <= 6'd17;
+			end
+			else if((global_count >= global_count_max) & (local_count_4 == 17)) begin
+				//restore Left servo to resting position so next move it back forward
+				right_servo_forward <= forward;
+				
+				global_count <= 0;
+				local_count_4 <= 0;
+			end
+		end
 								
-		global_count <= global_count + 27'd1;
+		global_count <= global_count + 27'b1;
 		
 	end
 	
